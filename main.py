c056e131733848d154677c5138827d2d1813a08c
Tuki1077 <70412089+Tuki1077@users.noreply.github.com>
2022-03-03 19:07:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import functions as fn
+import time 
 
 x = np.array( [
     ["","A","B","C","D","E","F"],
@@ -8,9 +9,11 @@ x = np.array( [
     ["3","0","0","0","0","0","0"]
 ])
 
-while opcion != 5:
+menu = True
+while menu == True:
     opcion = int(input(
         """
+
         1. Ver disponibilidad de boletos
         2. Reservar boleto de forma manual
         3. Reserva boleto aleatoriamente
@@ -22,14 +25,21 @@ while opcion != 5:
         for i in x:
             print('\t'.join(map(str, i)))
     elif opcion == 2:
-        user_column = input ("Ingrese la columna en la cual desea reservar su boleto: ")
-        user_row = int(input("Ingrese la fila en la que desea reservar un boleto: "))
-        translate = fn.translate_to_number(user_column)
-        reservacion = fn.reservar_boleto(x,user_row, translate)
+        y = int(input("Cuantos asientos desea reservar: "))
+        contador = 0
+        while contador < y:
+            user_column = input ("Ingrese la columna en la cual desea reservar su boleto: ")
+            user_row = int(input("Ingrese la fila en la que desea reservar un boleto: "))
+            translate = fn.translate_to_number(user_column)
+            reservacion = fn.reservar_boleto(x,user_row, translate)
+            contador = contador + 1
     elif opcion == 3:
-        pass
+        reservar_random = fn.reservar_random(x)
     elif opcion == 4:
         pass
+    elif opcion == 5:
+        print ("Gracias por utilizar nuestros servicios!...")
+        menu = False
 
 
 
