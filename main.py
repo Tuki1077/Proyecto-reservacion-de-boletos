95c316d53c24d1551d8caebd8e9765cbb239d67d
Tuki1077 <70412089+Tuki1077@users.noreply.github.com>
2022-03-03 18:00:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,33 @@ import functions as fn
 
 x = np.array( [
     ["","A","B","C","D","E","F"],
-    ["1","X","0","0","0","0","0"],
+    ["1","0","0","0","0","0","0"],
     ["2","0","0","0","0","0","0"],
     ["3","0","0","0","0","0","0"]
 ])
 
-for i in x:
-    print('\t'.join(map(str, i)))
-
-user_column = input ("Ingrese la columna en la cual desea reservar su boleto: ")
-user_row = int(input("Ingrese la fila en la que desea reservar un boleto: "))
-
-
-
-translate = fn.translate_to_number(user_column)
-print(translate)
-reservacion = fn.reservar_boleto(x,user_row, translate)
-print(reservacion)
+while opcion != 5:
+    opcion = int(input(
+        """
+        1. Ver disponibilidad de boletos
+        2. Reservar boleto de forma manual
+        3. Reserva boleto aleatoriamente
+        4. Desocupar asiento
+        5. Salir
+        """
+    ))
+    if opcion == 1:
+        for i in x:
+            print('\t'.join(map(str, i)))
+    elif opcion == 2:
+        user_column = input ("Ingrese la columna en la cual desea reservar su boleto: ")
+        user_row = int(input("Ingrese la fila en la que desea reservar un boleto: "))
+        translate = fn.translate_to_number(user_column)
+        reservacion = fn.reservar_boleto(x,user_row, translate)
+    elif opcion == 3:
+        pass
+    elif opcion == 4:
+        pass
 
 
 
