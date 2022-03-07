4c5dc54308b4ebfb319f59274bec4849639b7e12
Tuki1077 <70412089+Tuki1077@users.noreply.github.com>
2022-03-07 09:31:28 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ def reservar_boleto (x, user_row, user_column):
     if x[user_row, user_column] != "X":
         x[user_row, user_column] = "X"
     else:
-        print("Asiento ya esta ocupado, intente con otro asiento")
+        return("Asiento ya esta ocupado, intente con otro asiento")
     for i in x:
         print('\t'.join(map(str, i)))
 
